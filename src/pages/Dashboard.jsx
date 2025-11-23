9f6c5058823fabbc96d9dc55df991a572468efac
unknown <rakrakesh267@gmail.com>
2025-11-23 12:51:53 +0530

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='text-4xl font-bold text-red-500'>Dashboard</div>
+    <div className='text-4xl font-bold text-red-500'>Device Dashboard</div>
   )
 }
 
