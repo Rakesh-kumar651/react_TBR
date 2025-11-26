7d3a1464c79d955dbc931c7c7edf7ab75cc8547e
Rakesh-kumar651 <rakrakesh267@gmail.com>
2025-11-26 18:09:43 +0530

    layout change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import MainLayout from "./layout/MainLayout.jsx";
+import MainLayout1 from "./layout/MainLayout1.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import DeviceList from "./pages/Utilities/DeviceList.jsx";
 
@@ -14,8 +15,14 @@ function App() {
 
           <Route element={<MainLayout />}>
             <Route path="/" element={<Dashboard />} />
+            {/* <Route path="/utilities" element={<DeviceList />} /> */}
+          </Route>
+
+            <Route element={<MainLayout1 />}>
+            {/* <Route path="/" element={<Dashboard />} /> */}
             <Route path="/utilities" element={<DeviceList />} />
           </Route>
+
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
